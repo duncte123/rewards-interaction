0806ce3b7403bd5b50fb3f8a2f5552b014627ae9
duncte123 <contact@duncte123.me>
2021-05-04 17:49:23 +0200

    Model the twitch poll api

--- src/apis/types/twitchtypes.d.ts
+++ src/apis/types/twitchtypes.d.ts
@@ -20,3 +20,55 @@ export type TwitchChannel = {
   game_name: string;
   title: string;
 };
+
+export type TwitchPollCreateRequest = {
+  // required
+  broadcaster_id: string;
+  title: string;
+  choices: TwitchPollRequestChoice[];
+  duration: number;
+
+  // optional
+  bits_voting_enabled?: boolean;
+  bits_per_vote?: number;
+  channel_points_voting_enabled?: boolean;
+  channel_points_per_vote?: number;
+};
+
+export type TwitchPollRequestChoice = {
+  title: string;
+};
+
+export type TwitchPollTerminateRequest = {
+  broadcaster_id: string;
+  id: string;
+  status: 'TERMINATED' | 'ARCHIVED';
+};
+
+// Only 'TERMINATED' and 'ARCHIVED' are valid for ending a poll
+export type TwitchPollStatus = 'ACTIVE' | 'COMPLETED' | 'TERMINATED' | 'ARCHIVED' | 'MODERATED' | 'INVALID';
+
+export type TwitchPollResponse = {
+  id: string;
+  broadcaster_id: string;
+  broadcaster_name: string;
+  broadcaster_login: string;
+  title: string;
+  choices: TwitchPollResponseChoice[];
+  bits_voting_enabled: boolean;
+  bits_per_vote: number;
+  channel_points_voting_enabled: boolean;
+  channel_points_per_vote: number
+  status: TwitchPollStatus;
+  duration: number;
+  started_at: string;
+  ended_at?: string;
+};
+
+export type TwitchPollResponseChoice = {
+  id: string;
+  title: string;
+  votes: number;
+  channel_points_votes: number;
+  bits_votes: number;
+};
