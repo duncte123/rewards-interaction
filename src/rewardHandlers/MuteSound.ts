486d0754e50afc0c03d2aaa33ad0c7db6e5cdcc8
duncte123 <contact@duncte123.me>
2021-06-08 12:33:57 +0200

    Add icon to indicate if sound is muted

--- src/rewardHandlers/MuteSound.ts
+++ src/rewardHandlers/MuteSound.ts
@@ -6,8 +6,10 @@ import { sleep } from '../helpers.js';
 
 export default class MuteSound extends BaseHandler {
   async handle(user: string, reward: string, cost: string, message: string, extra: OnRewardExtra) {
+    await obs.setVisibilityOnSource('soundfx-images', 'audio_muted', true);
     await obs.toggleMute('soundcraft master');
     await sleep(30 * 1000);
     await obs.toggleMute('soundcraft master');
+    await obs.setVisibilityOnSource('soundfx-images', 'audio_muted', false);
   }
 }
